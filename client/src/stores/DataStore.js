09636c4460113be920f751395fb5dd3b54915316
carrotFarmer78 <caspardett@gmail.com>
2024-04-24 12:43:43 +0200

    Fixed wrong redirection

--- client/src/stores/DataStore.js
+++ client/src/stores/DataStore.js
@@ -1,32 +1,19 @@
 /* eslint-disable */
 
 import { defineStore } from 'pinia'
-import {onMounted, ref} from "vue";
-import {assertSimpleType} from "@babel/core/lib/config/caching";
+import { ref } from "vue";
 import axios from "axios";
 
 export const useDataStore = defineStore('data', () => {
     const items = ref([])
 
-    //TODO
-    if(process.env.UPDATE_ALL_ACCOUNTS == undefined){
-        var updateAllAccounts = 'http://localhost:3000/update/allAccounts'
-        var apiAllAccounts = 'http://localhost:3000/api/allAccounts'
-    }
-    else {
-        var updateAllAccounts = process.env.UPDATE_ALL_ACCOUNTS
-        var apiAllAccounts = process.env.API_ALL_ACCOUNTS
-    }
-
-    console.log(apiAllAccounts)
-
     async function updateData(){
-        const response = await axios.get(updateAllAccounts) //TODO
+        const response = await axios.get(`${process.env.SERVER}/api/AllAccounts/update`) //TODO
         return response.data
     }
 
     async function getAllAccounts(){
-        const response = await axios.get("https://leaguestatistics.onrender.com/api/allAccounts") //TODO
+        const response = await axios.get(`${process.env.SERVER}/api/AllAccounts`) //TODO
         items.value = response.data
     }
 
